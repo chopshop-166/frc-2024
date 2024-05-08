e6dc19dd9c10715a1ebd16445a40f41c55e124d9
Liam Murray <liammurray2015@icloud.com>
2024-05-07 20:49:44 -0400

    got rid of un-used buttons and made bare numbers variables in shooter

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -144,9 +144,6 @@ public class Robot extends CommandRobot {
                 }));
 
         driveController.y().whileTrue(drive.rotateToSpeaker());
-        driveController.a().whileTrue(drive.rotateToAngle(90));
-        driveController.b().whileTrue(drive.rotateToAngle(180));
-        driveController.x().whileTrue(drive.rotateToAngle(270));
 
         copilotController.back().onTrue(intake.safeStateCmd().andThen(armRotate.safeStateCmd()));
         copilotController.start().onTrue(shooter.setSpeed(Speeds.OFF));
